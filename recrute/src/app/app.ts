ea6d333270d51e85d0140fad52dda976b10baf95
u.martingoulet <ulricmartingoulet@gmail.com>
2026-03-29 14:37:01 +0200

    page d'accueil et login

--- recrute/src/app/app.ts
+++ recrute/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./navbar/navbar";
+import { RouterModule } from "@angular/router";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
