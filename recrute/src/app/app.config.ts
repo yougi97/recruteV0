14a563d8c0bdbe46a34835750aae55931a6096b1
Daphnee JEROME <daphnee.jerome@epfedu.fr>
2026-04-09 14:33:53 +0200

    change in path

--- recrute/src/app/app.config.ts
+++ recrute/src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
