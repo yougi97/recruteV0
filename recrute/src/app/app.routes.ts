87b6ba2d7224c82356a4c822e309e430257ebd5c
u.martingoulet <ulricmartingoulet@gmail.com>
2026-06-18 16:40:32 +0200

    contact

--- recrute/src/app/app.routes.ts
+++ recrute/src/app/app.routes.ts
@@ -13,6 +13,7 @@ import { CompanyOffers } from './company-offers/company-offers';
 import { MesCandidaturesComponent } from './mes-candidatures/mes-candidatures';
 import { MessagerieComponent } from './messagerie/messagerie';
 import { CandidatePublicView } from './candidate-public-view/candidate-public-view';
+import { Contact } from './contact/contact';
 
 export const routes: Routes = [
     { path: '', component: Home },
@@ -30,4 +31,5 @@ export const routes: Routes = [
     { path: 'about', component: AboutComponent },
     { path: 'company-offers', component: CompanyOffers },
     { path: 'messagerie', component: MessagerieComponent },
+    { path: 'contact',component: Contact },
 ];
